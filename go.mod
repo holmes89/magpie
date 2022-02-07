0386051a63ce2dab5143762cf7d2d90c297aae55
Joel Holmes <holmes89@gmail.com>
2022-02-06 20:28:21 -0500

    updates

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/holmes89/magpie
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
+
+require github.com/segmentio/ksuid v1.0.4 // indirect
---
+++ lib/handlers/rest/v1/resource.go
@@ -0,0 +1,111 @@
+package v1
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/holmes89/magpie/lib"
+
+	"github.com/segmentio/ksuid"
+)
+
+type ResourceService interface {
+	Get(context.Context, string, string) (lib.Resource, error)
+	GetAll(context.Context, string) ([]lib.Resource, error)
+	Create(context.Context, lib.Resource) error
+}
+
+type resourceHandler struct {
+	service ResourceService
+}
+
+func MakeV1ResourceHandler(mr *mux.Router, service ResourceService) http.Handler {
+
+	r := mr.PathPrefix("/r").Subrouter()
+
+	h := &resourceHandler{
+		service: service,
+	}
+
+	r.HandleFunc("/{resource}/{id}", h.Find).Methods("GET", "OPTIONS")
+	r.HandleFunc("/{resource}", h.FindAll).Methods("GET", "OPTIONS")
+	r.HandleFunc("/{resource}", h.Create).Methods("POST")
+
+	return r
+}
+func (h *resourceHandler) Find(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	vars := mux.Vars(r)
+	resource := vars["resource"]
+	id := vars["id"]
+
+	res, err := h.service.Get(ctx, resource, id)
+	if err != nil {
+		makeError(w, http.StatusBadRequest, "unable to find results", "get")
+		return
+	}
+
+	encodeResponse(r.Context(), w, res)
+}
+
+func (h *resourceHandler) FindAll(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	vars := mux.Vars(r)
+	resource := vars["resource"]
+
+	res, err := h.service.GetAll(ctx, resource)
+	if err != nil {
+		makeError(w, http.StatusBadRequest, "unable to find results", "get")
+		return
+	}
+
+	encodeResponse(r.Context(), w, res)
+}
+
+func (h *resourceHandler) Create(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	vars := mux.Vars(r)
+	resource := vars["resource"]
+
+	b, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	req := struct {
+		Name string                 `json:"name"`
+		Meta map[string]interface{} `json:"meta_data"`
+	}{}
+	if err := json.Unmarshal(b, &req); err != nil {
+		fmt.Println("unable to unmarshall resource")
+		makeError(w, http.StatusBadRequest, "unable to unmarshall resource", "post")
+	}
+
+	res := lib.Resource{
+		ID:        ksuid.New().String(),
+		Name:      req.Name,
+		Meta:      req.Meta,
+		Type:      resource,
+		CreatedAt: time.Now().UTC(),
+	}
+
+	if err := h.service.Create(ctx, res); err != nil {
+		fmt.Println("unable to create resource")
+		makeError(w, http.StatusBadRequest, "unable to create resource", "post")
+	}
+	w.WriteHeader(http.StatusCreated)
+	encodeResponse(ctx, w, res)
+}
+
+func makeError(w http.ResponseWriter, code int, message string, method string) {
+	fmt.Printf("HTTP Error: %d - %s - %s", code, method, message)
+	http.Error(w, message, code)
+}
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	return json.NewEncoder(w).Encode(response) //TODO check error and handle?
+}
